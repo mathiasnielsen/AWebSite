6e52369b804e7a463fa917a1403d58c7499079b1
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2018-06-19 18:43:54 +0200

    Second page

---
+++ scripts/diagnostics.js
@@ -0,0 +1,8 @@
+// Actually call the function to start it.
+function printStuff(text)
+{
+    console.log(text);
+
+    var txt = "Test";
+    document.getElementById("debug_text").innerHTML = txt;
+}
